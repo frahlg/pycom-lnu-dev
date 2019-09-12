d5b4192cbf7f09c6e8bac6e6a5c69faa5d4516c8
Fredrik Ahlgren <fredrik@ahlgren.nu>
2019-09-12 15:36:29 +0200

    pres

--- workshop-iot/ex2-onewire-dht11/main.py
+++ workshop-iot/ex2-onewire-dht11/main.py
@@ -1,18 +1,16 @@
 import time
 import ubinascii
 from machine import Pin
-from onewire import DS18X20
-from onewire import OneWire
+import dht
 
-#DS18B20 data line connected to pin P10
-ow = OneWire(Pin('P10'))
-temp = DS18X20(ow)
+dht_pin = Pin('P10', mode=Pin.OPEN_DRAIN)
+
 
 while True:
-
-    temp.start_conversion()
+    temp, hum = dht.DHT22(dht_pin)
+    temp_str = '{}.{}'.format(temp//10, temp % 10)
+    hum_str = '{}.{}'.format(hum//10, hum % 10)
     time.sleep(1)
-    t_ = temp.read_temp_async()
-    time.sleep(1)
-    print("DS18b20 temperature: " + str(t_))
+    print("DHT temperature: " + temp_str)
+    print("DHT RH: " + hum_str)
     time.sleep(5)
